ab822affc0fb3669eed95c24d5defd5b46c18b0b
John Alfred Velasco <113877494+alfred0907@users.noreply.github.com>
2022-10-05 14:03:51 +0800

    Export and test all functions to index.js (#2)
    Export and test all functions to index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib');
+const n = 5;
+const celsius = 37;
+const a = 5;
+const b = 10;
+const arrStrings = ['sample', 'Racecar'];
+
+console.log(`The square of ${n} is ${square(n)}.`);
+console.log(`${celsius}°C is equivalent to ${fahrenheit(celsius)}°F.`);
+console.log(`Area of rectangle (${a}, ${b}) = ${rectangleArea(a,b)}.`);
+for (let i = 0; i < arrStrings.length; i++) {
+   console.log(`${arrStrings[i]} is ${isPalindrome(arrStrings[i]) ? "" : "not "}a Palindrome.`);
+}
