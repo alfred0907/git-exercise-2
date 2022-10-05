90282ece71319fc00a5d46e6d4d4f8d88d873aa1
John Alfred Velasco <113877494+alfred0907@users.noreply.github.com>
2022-10-05 09:36:26 +0800

    Add functions (square, fahrenheit, rectangleArea, isPalindrome)

---
+++ lib.js
@@ -0,0 +1,12 @@
+const square = (n) => n * n;
+const fahrenheit = (celsius) => celsius * 1.8 + 32;
+const rectangleArea = (a, b) => a * b;
+const isPalindrome = (str) => {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i].toLowerCase() !== str[str.length - i - 1].toLowerCase()) {
+            return false;
+        }
+    }
+
+    return true;
+};
