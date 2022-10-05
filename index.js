8ed07b2af05450c35f00a9bf5b718f6f273b11d1
John Alfred Velasco <jadvelasco07@gmail.com>
2022-10-05 11:00:38 +0800

    Simplify palindrome output by using ternary operator

--- index.js
+++ index.js
@@ -9,9 +9,5 @@ console.log(`The square of ${n} is ${square(n)}.`);
 console.log(`${celsius}°C is equivalent to ${fahrenheit(celsius)}°F.`);
 console.log(`Area of rectangle (${a}, ${b}) = ${rectangleArea(a,b)}.`);
 for (let i = 0; i < arrStrings.length; i++) {
-    if (isPalindrome(arrStrings[i])) {
-        console.log(`${arrStrings[i]} is a Palindrome.`);
-    } else {
-        console.log(`${arrStrings[i]} is not a Palindrome.`);
-    }
+   console.log(`${arrStrings[i]} is ${isPalindrome(arrStrings[i]) ? "" : "not "}a Palindrome.`);
 }
